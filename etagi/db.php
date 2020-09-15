3560b3f03e6dd9292947f2c3542fa18217365639
egorQa96 <egorQa1996887@mail.ru>
2020-09-15 22:25:27 +0500

    Изменения для hetoku

--- etagi/db.php
+++ etagi/db.php
@@ -3,7 +3,7 @@
 require "libs/rb-mysql.php";
 
 // Подключаемся к БД
-R::setup( 'mysql:host=localhost;dbname=etagi', 'root', 'root' );
+R::setup( 'mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_8bcc022f59a9e52', 'b55848166756f5', '016d3c67' );
 // Проверка подключения к БД
 if(!R::testConnection()) die('No DB connection!');
 session_start(); //Создаем сессию для авторизации
